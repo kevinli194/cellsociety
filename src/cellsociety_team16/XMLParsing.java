a35af86d96e9437415c979249c9ebac002cc414d
Abhishek <abhishek.bend@gmail.com>
2014-09-14 09:38:32 -0400

    removed main chunk from xmlparsing.java, small main method included to run separately

--- src/cellsociety_team16/XMLParsing.java
+++ src/cellsociety_team16/XMLParsing.java
@@ -16,45 +16,6 @@ import java.io.IOException;
 
 public class XMLParsing {
 
-/*	public static void main(String argv[]) {
-
-		try {
-			File fXmlFile = new File("C:\\Users\\Abhishek B\\Documents\\workspace\\cellsociety_team16\\src\\cellsociety_team16\\xml\\initialGameParameters.xml");
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			Document doc = db.parse(fXmlFile);
-			doc.getDocumentElement().normalize();
-			System.out.println("Root element " + doc.getDocumentElement().getNodeName());
-			System.out.println("SIMULATION MODE: " + doc.getElementsByTagName("simulationMode").item(0).getChildNodes().item(0).getNodeValue());
-			System.out.println("GRID X SIZE: " + doc.getElementsByTagName("gridXSize").item(0).getChildNodes().item(0).getNodeValue());
-			System.out.println("GRID Y SIZE: " + doc.getElementsByTagName("gridYSize").item(0).getChildNodes().item(0).getNodeValue());
-			NodeList nodeLst = doc.getElementsByTagName("cell");
-			for (int s = 0; s < nodeLst.getLength(); s++) {
-
-				Node fstNode = nodeLst.item(s);
-
-				if (fstNode.getNodeType() == Node.ELEMENT_NODE) {
-
-					Element fstElmnt = (Element) fstNode;
-					NodeList fstNmElmntLst = fstElmnt.getElementsByTagName("state");
-					Element fstNmElmnt = (Element) fstNmElmntLst.item(0);
-					NodeList fstNm = fstNmElmnt.getChildNodes();
-					System.out.println("State : "  + ((Node) fstNm.item(0)).getNodeValue());
-					NodeList lstNmElmntLst = fstElmnt.getElementsByTagName("x");
-					Element lstNmElmnt = (Element) lstNmElmntLst.item(0);
-					NodeList lstNm = lstNmElmnt.getChildNodes();
-					System.out.println("X : " + ((Node) lstNm.item(0)).getNodeValue());
-					NodeList l2stNmElmntLst = fstElmnt.getElementsByTagName("y");
-					Element l2stNmElmnt = (Element) l2stNmElmntLst.item(0);
-					NodeList l2stNm = l2stNmElmnt.getChildNodes();
-					System.out.println("Y : " + ((Node) l2stNm.item(0)).getNodeValue());
-				}
-
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	} */
 
 /*	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException
 	{
